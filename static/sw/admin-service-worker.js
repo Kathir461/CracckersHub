c61da6767d3320194e139f6f371df835a7e98f39
kr0240-dev <kr0240@srmist.edu.in>
2026-07-01 10:44:40 +0530

    PWA update

---
+++ static/sw/admin-service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'crackershub-admin-v1';
+const APP_SHELL = [
+  '/admin',
+  '/admin/login',
+  '/static/css/styles.css',
+  '/static/js/floating_contact.js',
+  '/static/manifest-admin.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  if (event.request.url.startsWith('http') && !event.request.url.startsWith(self.location.origin)) return;
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match('/admin/login')))
+  );
+});
